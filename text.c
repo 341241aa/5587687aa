475ac57aff4604adee88632232a76d98c82a1970
AAAXJN <1577909711@qq.com>
2026-09-13 15:11:34 +0800

    补充提交

--- text.c
+++ text.c
@@ -1,12 +1,17 @@
 include<stdio.h>{
+	
 	typedef struct{
 	int x;
 	double y;
-	}
+	}AAA;
+
+
 	int main(void){
-
-	}
-
-
+    AAA a;
+	a.x = 10;
+	a.y = 3.14;
+	AAA* p = &a;
+	p -> x = 1;
+	p -> y = 2.01;
 return 0;
 }
